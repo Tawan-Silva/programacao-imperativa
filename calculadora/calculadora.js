3711d3d3c162ead0d252c61bc14d00bb1e22043c
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2021-09-01 20:42:49 -0300

    Atividade

---
+++ calculadora/calculadora.js
@@ -0,0 +1,55 @@
+function adicionar (Valor1, Valor2) {
+    return Valor1 + Valor2
+}
+
+
+function subtrair (Valor1, Valor2) {
+    return Valor1 - Valor2
+}
+
+function multiplicao (Valor1, Valor2) {
+    return Valor1 * Valor2
+}
+
+function divisao (Valor1, Valor2) {
+    return Valor1 / Valor2
+}
+
+console.log(adicionar(8, 9));
+
+console.log(subtrair(18, 14));
+
+
+// console.log("------ Teste de Operações / Calculadora ------")
+
+
+// console.log(adicionar(8, 9)); // SOMA
+
+// console.log(subtrair(80, 37)); // SUBTRAIR
+
+// console.log(multiplicao(7, 4)); // MULTIPLICAR
+
+// console.log(divisao(17, 0)); // DIVIDIR
+
+
+// console.log("------------ Funções Extras ------------")
+
+
+// function quadradoDoNumero(multiplicao) {
+//     return 
+// }
+
+// function mediaDeTresNumeros (Valor1, Valor2, Valor3) { // CALCULAR MEDIA
+//     return Valor1 + Valor2 + Valor3 / 3
+// }
+
+// console.log(mediaDeTresNumeros(10, 5, 9));
+
+
+// function calculaPorcentagem (Valor1, Valor2) {
+//     return Valor1 && Valor2 / 100
+// }
+
+// console.log(calculaPorcentagem(300, 15));
+
+
