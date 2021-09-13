076ff9a8596b4e2afc25a301736cb405622c1bd6
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2021-09-13 17:32:44 -0300

    Aula 11

---
+++ aula11/aula11.js
@@ -0,0 +1,90 @@
+// Escreva um aplicativo em Javascript que mostre todos os números ímpares de 1 até 100
+var contador = 0;
+
+while (contador < 100) {
+    if (contador % 2 === 1) {
+        console.log(contador + " Esse é Ímpar");
+    }
+    ++contador
+}
+
+contador = 0;
+
+while (contador < 100) {
+    if (contador % 2 === 0) {
+        console.log(contador + " Esse é Par");
+    }
+    ++contador
+}
+
+contador = 0
+
+function imparPar(contador) {
+    for (contador = 0; contador < 100; contador++) {
+        if (contador % 2 === 1) {
+            console.log(contador + " Esse é ímpar");
+        } else if (contador % 2 === 0) {
+            console.log(contador + " Esse é Par")
+        }
+    }
+}
+
+console.log(imparPar())
+
+var manhwas = ["Matial Peak", "Solo Leveling", "Tales of Demons and Gods"];
+
+function converterMaisculas(manhwas) {
+    manhwas[0] = manhwas[0].toUpperCase()
+    manhwas[1] = manhwas[1].toUpperCase()
+    manhwas[2] = manhwas[2].toUpperCase()
+    return manhwas
+}
+console.log(converterMaisculas(manhwas))
+
+var manhwas = ["Matial Peak", "Solo Leveling", "Tales of Demons and Gods"];
+var length = manhwas.length;
+// Escreva um aplicativo em Javascript que mostre todos os números ímpares de 1 até 100
+var contador = 0;
+
+while (contador < 100) {
+    if (contador % 2 === 1) {
+        console.log(contador + " Esse é Ímpar");
+    }
+    ++contador
+}
+
+contador = 0;
+
+while (contador < 100) {
+    if (contador % 2 === 0) {
+        console.log(contador + " Esse é Par");
+    }
+    ++contador
+}
+
+contador = 0
+
+function imparPar(contador) {
+    for (contador = 0; contador < 100; contador++) {
+        if (contador % 2 === 1) {
+            console.log(contador + " Esse é ímpar");
+        } else if (contador % 2 === 0) {
+            console.log(contador + " Esse é Par")
+        }
+    }
+}
+
+console.log(imparPar())
+
+var manhwas = ["Matial Peak", "Solo Leveling", "Tales of Demons and Gods"];
+
+function converterMaisculas(manhwas) {
+    manhwas[0] = manhwas[0].toUpperCase()
+    manhwas[1] = manhwas[1].toUpperCase()
+    manhwas[2] = manhwas[2].toUpperCase()
+    return manhwas
+}
+console.log(converterMaisculas(manhwas))
+
+var manhwas = ["Matial Peak", "Solo Leveling", "Tales of Demons and Gods"];
+var length = manhwas.length;
